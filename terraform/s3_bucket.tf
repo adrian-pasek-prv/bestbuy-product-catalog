58efb0b3d5e17a376737394ae61f16278e18d423
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-09-29 06:56:23 +0000

    Add s3_bucket config with terraform

---
+++ terraform/s3_bucket.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.s3_bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "s3_bucket_name" {
+  type = string
+  default = "s3_bucket_name"
+}
+
+variable "ingestion_iam_user" {
+  type = string
+  default = "ingestion_iam_user"
+}
+
+variable "ingestion_iam_role" {
+  type = string
+  default = "ingestion_iam_user"
+}
