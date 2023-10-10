abb990157c07f5734d15431038f09da2cc0db343
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-10-10 11:30:31 +0000

    Change output structure

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,17 @@
-output "data_engineer_iam_user" {
-  value = aws_iam_user.data_engineer_iam_user.arn
-}
-output "data_engineer_iam_user_access_key" {
-  value = aws_iam_access_key.data_engineer_iam_user_access_key.id
-}
-output "data_engineer_iam_user_secret_key" {
-  value = aws_iam_access_key.data_engineer_iam_user_access_key.secret
+output "secrets" {
+  value = {
+    "data-engineer-user": {
+      "name": aws_iam_user.data_engineer_iam_user.name,
+      "access_key": aws_iam_access_key.data_engineer_iam_user_access_key.id,
+      "access_secret": aws_iam_access_key.data_engineer_iam_user_access_key.secret
+    }
+  }
   sensitive = true
+}
+
+output "variables" {
+  value = {
+    "s3_bucket_name": aws_s3_bucket.s3_bucket.id,
+    "data_loader_role_arn": aws_iam_role.data_loader_role.arn
+  }
 }
