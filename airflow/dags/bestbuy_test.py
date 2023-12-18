4266deb499c0bda5775e6ec8093520e8c56e7b60
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-12-18 08:47:06 +0100

    Filter the response by only necessary fields

--- airflow/dags/bestbuy_test.py
+++ airflow/dags/bestbuy_test.py
@@ -1,13 +1,23 @@
 import json
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.providers.http.operators.http import HttpOperator
 from operators.http_to_s3 import HttpToS3Operator
 from airflow.models import Variable
 from datetime import datetime
 from datetime import timedelta
 import time
 
+API_KEY = Variable.get("bestbuy_api_key")
+PRODUCT_FIELD_LIST = [
+    "sku", "name", "type", "start_date", "active", "active_update_date",
+    "regular_price", "sale_price", "price_update_date", "on_sale", "url",
+    "upc", "category_path", "alternate_categories", "customer_review_count",
+    "customer_review_average", "customer_top_rated", "free_shipping",
+    "in_store_availability", "in_store_availability_update_date",
+    "item_update_date", "online_availability",
+    "online_availability_update_date", "release_date", "shipping_cost",
+    "manufacturer", "model_number", "dollar_savings", "percent_savings"
+]
 
 def filter_response(response, key):
     return [item.json().get(key) for item in response]
@@ -48,7 +58,7 @@ with DAG(
         task_id="test_task",
         http_conn_id="bestbuy-api",
         method="GET",
-        endpoint='products(categoryPath.id=abcat0700000&onSale=true&freeShipping=true&inStoreAvailability=true)?apiKey=vPfPIjFVswznNKfOB7R8BA9e&pageSize=100&format=json',
+        endpoint=f"products(categoryPath.id=abcat0700000)?apiKey={API_KEY}&show={','.join(PRODUCT_FIELD_LIST)}&pageSize=100&format=json",
         response_filter=lambda response: filter_response(response, "products"), 
         pagination_function=get_next_page,
         aws_conn_id="football-leagues-data-loader",
